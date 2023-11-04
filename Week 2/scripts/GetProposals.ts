02dfa4b5920de878b6141d2b14720dff757ce540
Outerspace Staking <65666364+scarletbright@users.noreply.github.com>
2023-11-04 14:21:49 -0300

    Create GetProposals.ts

---
+++ Week 2/scripts/GetProposals.ts
@@ -0,0 +1,52 @@
+import { ethers } from "ethers";
+import { Ballot, Ballot__factory } from "../typechain-types";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+async function main() {
+  // Receiving parameters
+  const parameters = process.argv.slice(2);
+  if (!parameters || parameters.length < 1)
+    throw new Error("Parameters not provided");
+  const contractAddress = parameters[0];
+
+  // Configuring the provider
+  const provider = new ethers.JsonRpcProvider(
+    process.env.RPC_ENDPOINT_URL ?? ""
+  );
+  const lastBlock = await provider.getBlock("latest");
+  console.log(`Last block number: ${lastBlock?.number}`);
+  const lastBlockTimestamp = lastBlock?.timestamp ?? 0;
+  const lastBlockDate = new Date(lastBlockTimestamp * 1000);
+  console.log(
+    `Last block timestamp: ${lastBlockTimestamp} (${lastBlockDate.toLocaleDateString()} ${lastBlockDate.toLocaleTimeString()})`
+  );
+
+  // Configuring the wallet
+  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY ?? "", provider);
+  console.log(`Using address ${wallet.address}`);
+  const balanceBN = await provider.getBalance(wallet.address);
+  const balance = Number(ethers.formatUnits(balanceBN));
+  console.log(`Wallet balance ${balance} ETH`);
+  if (balance < 0.01) {
+    throw new Error("Not enough ether");
+  }
+
+  // Attaching the smart contract using Typechain
+  const ballotFactory = new Ballot__factory(wallet);
+  const ballotContract = ballotFactory.attach(contractAddress) as Ballot;
+
+  // Printing out the contract address
+  console.log(`Contract address: ${contractAddress}`);
+
+  // Printing out the proposals
+  for (let i = 0; i < 4; i++) {
+    const proposal = await ballotContract.proposals(i);
+    console.log(`Proposal ${i} ${ethers.decodeBytes32String(proposal.name)}`);
+  }
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
